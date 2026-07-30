880b887927aa88808f245c80f21a3ef90aa2a7e8
maslina524 <luk.kirill2804@gmail.com>
2026-07-31 01:26:45 +0300

    Updated module version

--- build.rs
+++ build.rs
@@ -1,4 +1,31 @@
+use std::process::Command;
+
 fn main() {
-    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    println!("cargo:rustc-env=TARGET_ARCH={}", arch);
+    // ENV: TARGET_ARCH
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    println!("cargo:rustc-env=TARGET_ARCH={}", target_arch);
+
+    // ENV: BUILD_TIME
+    let build_time = chrono::Local::now().format("%b %d %Y, %H:%M:%S").to_string();
+    println!("cargo:rustc-env=COMPILE_TIME={}", build_time);
+
+    // ENV: TARGET_OS
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    println!("cargo:rustc-env=TARGET_OS={}", target_os);
+
+    // ENV: RUSTC_VERSION
+    let mut rustc_version = Command::new("rustc")
+        .arg("--version")
+        .output()
+        .ok()
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .unwrap_or_else(|| "unknown".to_string());
+    
+    if let Some(idx) = rustc_version.find("(") {
+        rustc_version = rustc_version[..idx - 1].trim().to_string()
+    } 
+
+    println!("cargo:rustc-env=RUSTC_VERSION={}", rustc_version.trim());
+
+    println!("cargo:rerun-if-changed=build.rs");
 }
--- src/modules/version.rs
+++ src/modules/version.rs
@@ -1,21 +1,53 @@
+/// EXAMPLE:
+/// 
+/// project-name: fastfetch
+/// version: 2.66.0
+/// version-tweak:  
+/// build-type: release 
+/// sysname: Windows 
+/// arch: x86_64 
+/// cmake-built-type: RelWithDebInfo 
+/// compile-time: Jul 10 2026, 07:10:53 
+/// compiler: clang 22.1.8 
+/// libc: ucrt 14.0
+#[derive(Debug)]
 pub struct Version<'a> {
-    name: &'a str,
-    ver: &'a str,
-    arch: &'a str
+    project_name: &'a str,
+    version: &'a str,
+    version_tweak: &'a str,
+    build_type: &'a str,
+    sysname: &'a str,
+    arch: &'a str,
+    cmake_built_type: &'a str,
+    compile_time: &'a str,
+    compiler: &'a str,
+    libc: &'a str
 }
 
 impl<'a> Version<'a> {
     pub fn new() -> Self {
+        let build_type = if cfg!(debug_assertions) {
+            "debug"
+        } else {
+            "release"
+        };
         Self {
-            name: env!("CARGO_BIN_NAME"), 
-            ver: env!("CARGO_PKG_VERSION"), 
-            arch: env!("TARGET_ARCH")
+            project_name: env!("CARGO_BIN_NAME"), 
+            version: env!("CARGO_PKG_VERSION"), 
+            version_tweak: "", 
+            build_type, 
+            sysname: env!("TARGET_OS"),
+            arch: env!("TARGET_ARCH"), 
+            cmake_built_type: "", 
+            compile_time: env!("COMPILE_TIME"), 
+            compiler: env!("RUSTC_VERSION"), 
+            libc: ""
         }
     }
 }
 
 impl<'a> core::fmt::Display for Version<'a> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{} {} ({})", self.name, self.ver, self.arch)
+        write!(f, "{} {} ({})", self.project_name, self.version, self.arch)
     }
 }
