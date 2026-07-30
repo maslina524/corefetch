b4a795aa9fee7a3928469dbac91d4d6a4a961ea1
maslina524 <luk.kirill2804@gmail.com>
2026-07-30 19:37:13 +0300

    Added no_std attr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![deny(clippy::all)]
+
+use core::ffi::c_int;
+
+// #[global_allocator]
+// static ALLOCATOR: Allocator = Allocator;
+
+#[cfg(not(test))]
+mod panic_impl {
+    use core::panic::PanicInfo;
+
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+    }
+}
+
+// #[cfg(not(test))]
+#[unsafe(no_mangle)]
+extern "C" fn main() -> c_int {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+
+    #[test]
+    fn test_test() {
+        std::println!("Hello World!");
+    }
 }
