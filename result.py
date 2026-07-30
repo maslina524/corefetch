12f64910ba7e315cf1642e0eb3a16784ac74b566
maslina524 <luk.kirill2804@gmail.com>
2026-07-31 01:48:19 +0300

    Added result.py

---
+++ result.py
@@ -0,0 +1,18 @@
+import json
+import subprocess
+import sys
+from pprint import pprint
+
+cmd = subprocess.run(["fastfetch", "--json", "-c", "presets/all.jsonc"], capture_output=True, text=True)
+stdout = cmd.stdout
+out_json = json.loads(stdout)
+
+args = sys.argv
+
+for module in out_json:
+    if module["type"].lower() == args[1].lower():
+        result = module["result"]
+        for k in list(result.keys()):
+            print(f"{k}: {result[k]}")
+
+        sys.exit(0)
