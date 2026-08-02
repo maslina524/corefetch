21bdf582a2b52f24c05d77ff098c5b7a95efbeda
maslina524 <luk.kirill2804@gmail.com>
2026-08-02 19:35:27 +0300

    Added fn utf8_to_utf16 in os/encoding

--- src/os/encoding.rs
+++ src/os/encoding.rs
@@ -7,7 +7,7 @@ use alloc::{
 
 use crate::os::{
     error::{self, ErrorCode},
-    windows::WideCharToMultiByte
+    windows::{WideCharToMultiByte, MultiByteToWideChar}
 };
 
 const CP_UTF8: u32 = 65001;
@@ -58,3 +58,43 @@ pub fn utf16le_to_utf8(
     Ok(string)
 }
 
+pub fn utf8_to_utf16le(
+    src: impl Into<String>
+) -> error::Result<Vec<u16>> {
+    // SAFETY: Passing invalid UTF-16-LE bytes will panic;
+    // you should only use this function when you are 
+    // absolutely sure the bytes are completely valid,
+    // or for converting strings created by the WinAPI.
+    
+    let src = src.into();
+    let size = unsafe {
+        MultiByteToWideChar(
+            CP_UTF8, 
+            0, 
+            src.as_ptr(), 
+            src.len() as i32, 
+            ptr::null_mut(), 
+            0,
+        ) 
+    };
+    if size == 0 { 
+        return Err(ErrorCode::last()) 
+    }
+
+    let mut buf = Vec::with_capacity(size as usize);
+    let ret = unsafe {
+        MultiByteToWideChar(
+            CP_UTF8, 
+            0, 
+            src.as_ptr(), 
+            src.len() as i32, 
+            buf.as_mut_ptr(), 
+            size
+        ) 
+    };
+    if ret == 0 { 
+        return Err(ErrorCode::last()) 
+    }
+    
+    Ok(buf)
+}
