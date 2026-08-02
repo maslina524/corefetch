cab5fa9de5d234882e5bc7f93abdb4becbfdfd14
maslina524 <luk.kirill2804@gmail.com>
2026-08-02 23:12:00 +0300

    New module colors

---
+++ src/modules/colors.rs
@@ -0,0 +1,31 @@
+use crate::sync::OnceLock;
+
+static COLORS: OnceLock<Colors> = OnceLock::new();
+
+pub fn get() -> &'static Colors {
+    COLORS.get_or_init(|| {
+        Colors::new()
+    })
+}
+
+#[derive(Debug)]
+pub struct Colors;
+
+impl Colors {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl core::fmt::Display for Colors {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        for i in 40..=47 {
+            let _ = write!(f, "\x1b[{i}m   ");
+        }
+        let _ = writeln!(f, "\x1b[0m");
+        for i in 100..=107 {
+            let _ = write!(f, "\x1b[{i}m   ");
+        }
+        write!(f, "\x1b[0m")
+    }
+}
