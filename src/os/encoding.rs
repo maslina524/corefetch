dc2084ccf728bed93b28591d8aed464d6c2c59c5
maslina524 <luk.kirill2804@gmail.com>
2026-08-02 19:17:22 +0300

    Another update for utf16le_to_utf8

--- src/os/encoding.rs
+++ src/os/encoding.rs
@@ -1,28 +1,60 @@
 use core::ptr;
 
-use crate::os::windows::WideCharToMultiByte;
+use alloc::{
+    string::String,
+    vec::Vec
+};
+
+use crate::os::{
+    error::{self, ErrorCode},
+    windows::WideCharToMultiByte
+};
 
 const CP_UTF8: u32 = 65001;
-const DEFAULT: u8  = b'?';
 
-pub unsafe fn utf16le_to_utf8(src: &[u16], src_len: isize, dst: &mut [u8], dst_size: usize) -> usize {
+pub fn utf16le_to_utf8(
+    src: &[u16], 
+    len: isize,
+) -> error::Result<String> {
     // SAFETY: Passing invalid UTF-16-LE bytes will panic;
     // you should only use this function when you are 
     // absolutely sure the bytes are completely valid,
     // or for converting strings created by the WinAPI.
+    
+    let size = unsafe {
+        WideCharToMultiByte(
+            CP_UTF8, 
+            0, 
+            src.as_ptr(), 
+            len as i32, 
+            ptr::null_mut(), 
+            0, 
+            ptr::null(), 
+            ptr::null_mut()
+        ) 
+    };
+    if size == 0 { 
+        return Err(ErrorCode::last()) 
+    }
 
+    let mut buf = Vec::with_capacity(size as usize);
     let ret = unsafe {
         WideCharToMultiByte(
             CP_UTF8, 
             0, 
-            src as *const _ as *const u16, 
-            src_len as i32, 
-            dst.as_mut_ptr(), 
-            dst_size as i32, 
-            &DEFAULT, 
+            src.as_ptr(), 
+            len as i32, 
+            buf.as_mut_ptr(), 
+            size, 
+            ptr::null(), 
             ptr::null_mut()
         ) 
     };
-    if ret == 0 { panic!("WideCharToMultiByte error!"); }
-    ret as usize
+    if ret == 0 { 
+        return Err(ErrorCode::last()) 
+    }
+    
+    let string = String::from_utf8(buf).unwrap();
+    Ok(string)
 }
+
