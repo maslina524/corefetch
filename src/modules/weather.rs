4d542f214cb2c701ed8bb171ac02f391911e1ae8
maslina524 <luk.kirill2804@gmail.com>
2026-08-05 20:39:39 +0300

    Now Weather is cached on the pc

--- src/modules/weather.rs
+++ src/modules/weather.rs
@@ -1,12 +1,19 @@
-use alloc::string::String;
+use alloc::{
+    string::String,
+    vec::Vec,
+    borrow::ToOwned
+};
 
 use crate::{
     sync::OnceLock,
     os::https::Request,
+    os::path::Path,
+    os::fs::{self, File, Access},
+    os::env,
     format
 };
 
-const WTTR_URL: &str = "https://wttr.in/?lang=en&format=%t %E2%80%94 %C (%l)";
+const WTTR_URL: &str = "https://wttr.in/?lang=en&format=%t+%E2%80%94+%C+(%l)";
 
 static WEATHER: OnceLock<Weather> = OnceLock::new();
 
@@ -17,14 +24,58 @@ pub struct Weather {
 
 impl Weather {
     pub fn new() -> Self {
-        let response = Request::new(WTTR_URL).unwrap().get();
-        let result = if response.is_success() {
-            response.as_text().unwrap()
+        let cur_hour = env::timestamp_hours();
+        let result = if let Some((hours, data)) = Self::read_cache() {
+            if cur_hour == hours {
+                data
+            } else {
+                let data = Self::request();
+                Self::set_cache(cur_hour, &data);
+                data
+            }
         } else {
-            format!("Unknown: code {}", response.code())
+            let data = Self::request();
+            Self::set_cache(cur_hour, &data);
+            data
         };
 
         Self { result }
+    }
+
+    fn request() -> String {
+        let response = Request::new(WTTR_URL).unwrap().get();
+        if response.is_success() {
+            response.as_text().unwrap()
+        } else {
+            format!("Response code: {}", response.code())
+        }
+    }
+
+    fn read_cache() -> Option<(u64, String)> {
+        let path = Path::cache().join("weather");
+        let string = fs::read_to_string(path).ok()?;
+
+        let parts: Vec<&str> = string.splitn(2, '\n').collect();
+        if parts.len() != 2 {
+            return None
+        }
+
+        let hours = parts[0].parse::<u64>().ok()?;
+        let data = parts[1].to_owned();
+        Some((hours, data))
+    }
+
+    fn set_cache(hour: u64, data: &str) -> Option<()> {
+        let path_dir = Path::cache();
+        fs::create_dirs(&path_dir).ok()?;
+
+        let path = path_dir.join("weather");
+        let file = File::create_always(path, Access::Write).ok()?;
+
+        let formatted = format!("{hour}\n{data}");
+        file.write(formatted).ok()?;
+
+        Some(())
     }
 
     pub fn get() -> &'static Self {
