acede1a637bec0bd374beec1964bda2af777fb35
maslina524 <luk.kirill2804@gmail.com>
2026-08-07 00:19:16 +0300

    Added many macros in modules/mod

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -12,20 +12,79 @@ pub use processes::Processes;
 pub use version::Version;
 pub use weather::Weather;
 
+#[derive(Default)]
 pub struct FormatValue<'a> {
     pub format: Option<&'a str>,
     pub color: Option<&'a str>
 }
 
-impl<'a> Default for FormatValue<'a> {
-    fn default() -> Self {
-        Self { format: None, color: None }
-    }
-}
-
 pub trait Module {
     fn new() -> Self;
     fn get() -> &'static Self;
-    fn key() -> alloc::string::String;
-    fn format(key: FormatValue, format: FormatValue) -> alloc::string::String;
+    fn key() -> &'static str;
+    fn title() -> &'static str;
+    fn format(&self, key: FormatValue, format: FormatValue) -> alloc::string::String;
+}
+
+#[macro_export]
+macro_rules! impl_display_for_module {
+    ($name:ident) => {
+        impl core::fmt::Display for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                write!(f, "{}", self.format($crate::modules::FormatValue::default(), $crate::modules::FormatValue::default()))
+            }
+        }
+    };
+    ($name:ident, $lt:lifetime) => {
+        impl core::fmt::Display for $name<$lt> {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                write!(f, "{}", self.format($crate::modules::FormatValue::default(), $crate::modules::FormatValue::default()))
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! format_for_module {
+    ($name:ident, $($field:ident),*) => {
+        fn format(&self, key: super::FormatValue, format: super::FormatValue) -> alloc::string::String {
+            let key_color = key.color.unwrap_or($crate::logo::LogoInfo::get().unwrap().color_keys);
+            let key_format = key.format.unwrap_or($name::key());
+            let key_string = $crate::format_module!(key_format, self, $($field),*);
+
+            let value_color = format.color.unwrap_or($crate::logo::LogoInfo::get().unwrap().color_title);
+            let value_format = format.format.unwrap_or($name::title());
+            let value_string = $crate::format_module!(value_format, self, $($field),*);
+
+            $crate::format!("\x1b[{key_color}m{key_string}\x1b[0m: \x1b[{value_color}m{value_string}\x1b[0m")
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! format_module {
+    ($format:expr, $obj:ident, $($field:ident),*) => {{
+        let mut result = alloc::string::ToString::to_string($format);
+        let mut idx = 1;
+        
+        $(
+            result = result.replace(
+                &alloc::fmt::format(format_args!("{{{}}}", stringify!($field))),
+                &$crate::format_module!(@to_string &$obj.$field)
+            );
+            
+            result = result.replace(
+                &alloc::fmt::format(format_args!("{{{}}}", idx)),
+                &$crate::format_module!(@to_string &$obj.$field)
+            );
+            
+            idx += 1;
+        )*
+        
+        result
+    }};
+    
+    (@to_string $expr:expr) => {{
+        alloc::string::ToString::to_string($expr)
+    }};
 }
