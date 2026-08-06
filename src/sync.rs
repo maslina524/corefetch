d695c7bec27aff7aadcfd829ee69e809c9662d68
maslina524 <luk.kirill2804@gmail.com>
2026-08-06 22:34:41 +0300

    Added fn get in OncleLock

--- src/sync.rs
+++ src/sync.rs
@@ -50,6 +50,15 @@ impl<T> OnceLock<T> {
             unsafe { (&*self.value.get()).assume_init_ref() }
         }
     }
+
+    pub fn get(&self) -> Option<&T> {
+        if self.state.load(Ordering::Acquire) == READY {
+            // SAFETY: The value in `MaybeUninit` is guaranteed to be initialized
+            unsafe { return Some((&*self.value.get()).assume_init_ref()); }
+        } else {
+            None
+        }
+    }
 }
 
 // SAFETY: trait is empty
