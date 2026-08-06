869bb2862fa8cdbeab15b9c16985380daa6dbc83
maslina524 <luk.kirill2804@gmail.com>
2026-08-06 22:17:49 +0300

    Added fn get

--- src/logo/mod.rs
+++ src/logo/mod.rs
@@ -7,20 +7,22 @@ use alloc::{
 
 use crate::{
     color,
-    format
+    format,
+    sync::OnceLock
 };
 
 mod w;
 
-pub const UNKNOWN: &str = include_str!("_/unknown.txt");
-pub const UNKNOWN_LOGO: LogoInfo = LogoInfo {
+const UNKNOWN: &str = include_str!("_/unknown.txt");
+static UNKNOWN_PTR: &LogoInfo = &LogoInfo {
     names: vec![], 
     lines: UNKNOWN, 
     colors: vec![], 
     color_keys: color::FG_DEFAULT, 
     color_title: color::FG_DEFAULT
 };
-pub static UNKNOWN_PTR: &LogoInfo = &UNKNOWN_LOGO;
+
+static LOGO_INFO: OnceLock<&'static LogoInfo> = OnceLock::new();
 
 pub struct LogoInfo {
     names: Vec<&'static str>,
@@ -32,20 +34,26 @@ pub struct LogoInfo {
 
 impl LogoInfo {
     pub fn new(name: &str) -> &Self {
-        let first_char = name.chars().next().unwrap();
+        LOGO_INFO.get_or_init(|| {
+            let first_char = name.chars().next().unwrap();
 
-        let stack = match first_char {
-            'w' | 'W' => w::get(),
-            _ => return UNKNOWN_PTR
-        };
+            let stack = match first_char {
+                'w' | 'W' => w::get(),
+                _ => return UNKNOWN_PTR
+            };
 
-        for logo in stack {
-            if logo.names.contains(&name)  {
-                return logo;
+            for logo in stack {
+                if logo.names.contains(&name)  {
+                    return logo;
+                }
             }
-        }
 
-        UNKNOWN_PTR
+            UNKNOWN_PTR
+        })
+    }
+
+    pub fn get() -> Option<&'static Self> {
+        LOGO_INFO.get().map(|v| &**v)
     }
 
     pub fn get_ansi_lines(&self) -> Vec<(String, usize)> {
