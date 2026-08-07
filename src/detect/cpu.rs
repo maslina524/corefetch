e39471a5ccad2cd3bbc422f4646c763c65fb1db0
maslina524 <luk.kirill2804@gmail.com>
2026-08-07 22:22:23 +0300

    Impl fn technology in detect/cpu

--- src/detect/cpu.rs
+++ src/detect/cpu.rs
@@ -6,6 +6,7 @@ use core::{
 
 use alloc::{
     string::String,
+    borrow::ToOwned,
     vec::Vec,
     vec
 };
@@ -128,9 +129,35 @@ pub const fn code_name() -> String {
     }
 }
 
-pub const fn technology() -> String {
+pub fn technology() -> String {
     if cfg!(any(target_arch = "x86_64", target_arch = "x86")) {
-        todo_or!("fn technology is not implemented", String::new())
+        let eax = __cpuid(1).eax;
+        let mut model = (eax >> 4) & 0x0F;
+        let mut family = (eax >> 8) & 0xF;
+
+        if family == 0xF {
+            family += (eax >> 20) & 0xFF;
+        }
+
+        if family == 0x6 {
+            model += ((eax >> 16) & 0xF) << 4;
+        }
+
+        if family != 0x6 {
+            return String::new();
+        }
+
+        match model {
+            0x97 | 0x9A | 0xB7 | 0xBA | 0xBE => "Intel 7",
+            0xAA => "Intel 4",
+            0xC7 | 0xD7 => "Intel 20A / 18A",
+            0x7D | 0x7E => "Intel 10nm (Ice Lake)",
+            0x8C | 0x8D => "Intel 10nm SuperFin (Tiger Lake)",
+            0x8E | 0x9E | 0xA7 => "Intel 14nm (Rocket Lake / Coffee Lake)",
+            0x4E | 0x5E => "Intel 14nm (Skylake)",
+            0x3C | 0x3D => "Intel 22nm (Haswell)",
+            _ => ""
+        }.to_owned()
     } else {
         String::new()
     }
@@ -167,5 +194,12 @@ mod tests {
         let numa = cpu::numa_nodes_count();
         assert!(numa != 0);
         println!("{numa}");
+    }
+
+    #[test]
+    fn technology_test() {
+        let tech = cpu::technology();
+        assert!(!tech.is_empty());
+        println!("{tech}");
     }
 }
