81f46f0364ef13991511c37c674145338cb77c3d
maslina524 <luk.kirill2804@gmail.com>
2026-08-08 02:39:54 +0300

    Added fns user_id & pid in detect/title

--- src/detect/title.rs
+++ src/detect/title.rs
@@ -1,20 +1,46 @@
-use alloc::string::String;
-
-use crate::{
-    os::windows::{GetUserNameW, GetUserNameExW, GetCurrentDirectoryW, GetComputerNameW},
-    os::path::Path,
-    os::env,
-    sync::OnceLock,
-    logo::LogoInfo,
-    todo_or,
-    format
+use core::{
+    ptr,
+    slice
 };
 
-static USER_NAME: OnceLock<String> = OnceLock::new();
-static HOST_NAME: OnceLock<String> = OnceLock::new();
+use alloc::{
+    string::String,
+    vec::Vec
+};
 
-const UNLEN     : usize            = 256;
-const PCNAME_LEN: usize            = 15;
+use crate::{
+    format,
+    logo::LogoInfo,
+    os::env,
+    os::path::Path,
+    os::windows::{
+        ConvertSidToStringSidW, GetComputerNameW, GetCurrentDirectoryW, GetCurrentProcess,
+        GetCurrentProcessId, GetTokenInformation, GetUserNameExW, GetUserNameW, OpenProcessToken,
+        LocalFree, CloseHandle, PSID
+    },
+    sync::OnceLock,
+    todo_or
+};
+
+static USER_NAME : OnceLock<String> = OnceLock::new();
+static HOST_NAME : OnceLock<String> = OnceLock::new();
+
+const UNLEN      : usize            = 256;
+const PCNAME_LEN : usize            = 15;
+const TOKEN_QUERY: u32              = 8;
+
+#[repr(C)]
+#[derive(Clone)]
+struct SID_AND_ATTRIBUTES {
+    pub sid: PSID,
+    pub attributes: u32,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+struct TOKEN_USER {
+    pub user: SID_AND_ATTRIBUTES,
+}
 
 pub fn user_name() -> &'static String {
     USER_NAME.get_or_init(|| {
@@ -121,6 +147,111 @@ pub fn cwd() -> String {
     String::from_utf16_lossy(&buf)
 }
 
+pub fn pid() -> u32 {
+    // SAFETY: Completely safe
+    unsafe {
+        GetCurrentProcessId()
+    }
+}
+
+pub fn user_id() -> String {
+    // SAFETY: Completely safe
+    let cur_proc = unsafe { GetCurrentProcess() };
+    let mut token  = ptr::null_mut();
+
+    // SAFETY: Completely safe
+    let ret = unsafe {
+        OpenProcessToken(
+            cur_proc, 
+            TOKEN_QUERY, 
+            &raw mut token
+        )
+    };
+    if ret == 0 {
+        return String::new();
+    }
+
+    let mut size = 0;
+    // SAFETY: Completely safe
+    unsafe {
+        GetTokenInformation(
+            token, 
+            1, 
+            ptr::null_mut(), 
+            0, 
+            &raw mut size
+        ) 
+    };
+    if size == 0 {
+        // SAFETY: Completely safe
+        unsafe { CloseHandle(token) };
+        return String::new();
+    }
+
+    let mut buffer: Vec<TOKEN_USER> = Vec::with_capacity(size as usize);
+    // SAFETY: Completely safe
+    let ret = unsafe {
+        GetTokenInformation(
+            token,
+            1,
+            buffer.as_mut_ptr().cast(),
+            size,
+            &raw mut size,
+        )
+    };
+    if ret == 0 {
+        // SAFETY: Completely safe
+        unsafe { CloseHandle(token) };
+        return String::new();
+    }
+
+    let ptoken_user = buffer.as_ptr().cast::<TOKEN_USER>();
+    // SAFETY: WinAPI returns a valid pointer to `TOKEN_USER`
+    let token_user = unsafe {
+        (*ptoken_user).clone()
+    };
+    let sid = token_user.user.sid;
+
+    let mut sid_str = ptr::null_mut();
+    // SAFETY: Completely safe
+    let ret = unsafe {
+        ConvertSidToStringSidW(
+            sid,
+            &raw mut sid_str
+        )
+    };
+    if ret == 0 {
+        // SAFETY: Completely safe
+        unsafe { CloseHandle(token) };
+        return String::new();
+    }
+
+    let result = if sid_str.is_null() {
+        String::new()
+    } else {
+        let mut len = 0;
+        // SAFETY: Up to the `\0`, this is a string allocated by WinAPI
+        while unsafe { *sid_str.add(len) } != 0 {
+            len += 1;
+        }
+
+        // SAFETY: Creating a string from memory allocated by WinAPI
+        let slice = unsafe {
+            slice::from_raw_parts(sid_str, len)
+        };
+
+        String::from_utf16_lossy(slice)
+    };
+
+    // SAFETY: Completely safe
+    unsafe {
+        LocalFree(sid_str.cast());
+        CloseHandle(token);
+    }
+    
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use crate::detect::title;
@@ -150,5 +281,11 @@ mod tests {
     fn host_test() {
         let cwd = title::host_name();
         println!("{cwd}");
+    }
+
+    #[test]
+    fn user_id_test() {
+        let user_id = title::user_id();
+        println!("{user_id}");
     }
 }
