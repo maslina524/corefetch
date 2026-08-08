126f3c5dc3a829f5018b8e7c1672d76563ddc59e
maslina524 <luk.kirill2804@gmail.com>
2026-08-08 13:58:52 +0300

    New fn month_name for Date

--- src/detect/datetime.rs
+++ src/detect/datetime.rs
@@ -30,23 +30,24 @@ impl core::fmt::Display for AmPm {
 
 #[derive(Debug)]
 pub struct Date {
-    year: u16,
-    month: u16,
+    pub year: u16,
+    pub month: u16,
+    pub month_name: String,
 
-    hour: u16,
-    minute: u16,
-    second: u16,
+    pub hour: u16,
+    pub minute: u16,
+    pub second: u16,
 
-    week: u16,
-    weekday: String,
-    weekday_short: String,
-    day_in_year: u16,
-    day_in_month: u16,
-    day_in_week: u16,
+    pub week: u16,
+    pub weekday: String,
+    pub weekday_short: String,
+    pub day_in_year: u16,
+    pub day_in_month: u16,
+    pub day_in_week: u16,
 
-    offset_utc: String,
-    timezone_name: String,
-    am_pm: AmPm
+    pub offset_utc: String,
+    pub timezone_name: String, 
+    pub am_pm: AmPm
 }
 
 impl Date {
@@ -57,6 +58,7 @@ impl Date {
 
         let year = time.wYear;
         let month = time.wMonth;
+        let month_name = Self::month_name(time.wMonth);
 
         let hour = time.wHour;
         let minute = time.wMinute;
@@ -64,8 +66,8 @@ impl Date {
 
         let day_in_year = Self::day_in_year(&time);
         let week = day_in_year / 7 + 1;
-        let weekday = Self::week_of_day(time.wDayOfWeek);
-        let weekday_short = Self::week_of_day_short(time.wDayOfWeek);
+        let weekday = Self::day_of_week(time.wDayOfWeek);
+        let weekday_short = Self::day_of_week_short(time.wDayOfWeek);
 
         let day_in_month = time.wDay;
         let day_in_week = time.wDayOfWeek;
@@ -79,14 +81,32 @@ impl Date {
         };
 
         Self {
-            year, month, hour,
-            minute, second, week, weekday,
-            weekday_short,  day_in_year, day_in_month, day_in_week,
-            offset_utc, timezone_name, am_pm
+            year, month, month_name,
+            hour, minute, second, week,
+            weekday, weekday_short, day_in_year, day_in_month,
+            day_in_week, offset_utc, timezone_name, am_pm
         }
     }
 
-    fn week_of_day(num: u16) -> String {
+    fn month_name(num: u16) -> String {
+        match num {
+            1  => "January",
+            2  => "February",
+            3  => "March",
+            4  => "April",
+            5  => "May",
+            6  => "June",
+            7  => "July",
+            8  => "August",
+            9  => "September",
+            10 => "October",
+            11 => "November",
+            12 => "December",
+            _ => unreachable!()
+        }.to_owned()
+    }
+
+    fn day_of_week(num: u16) -> String {
         match num {
             0 => "Sunday",
             1 => "Monday",
@@ -99,7 +119,7 @@ impl Date {
         }.to_owned()
     }
 
-    fn week_of_day_short(num: u16) -> String {
+    fn day_of_week_short(num: u16) -> String {
         match num {
             0 => "Sun",
             1 => "Mon",
