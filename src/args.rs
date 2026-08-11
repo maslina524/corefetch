8b3d9eebb72a0d2f9fe3986b942bc71b91d6e1a1
maslina524 <luk.kirill2804@gmail.com>
2026-08-11 19:16:35 +0300

    Updated args

--- src/args.rs
+++ src/args.rs
@@ -2,36 +2,34 @@ use alloc::collections::BTreeMap;
 use alloc::string::String;
 use alloc::vec::Vec;
 use alloc::borrow::ToOwned;
-use alloc::boxed::Box;
 use alloc::format;
 
 #[derive(Debug, Clone)]
 struct Arg {
     long: String,
     short: Option<char>,
-    takes_value: bool
+    takes_value: bool,
 }
 
 #[derive(Debug, Clone)]
 pub struct ArgsParser {
     command_name: String,
     description: String,
-    arguments: Vec<Arg>
+    arguments: Vec<Arg>,
 }
 
 #[derive(Debug, Clone)]
 pub struct ArgResult {
     pub map: BTreeMap<String, String>,
-    pub nn: Vec<String>,
-    pub action: Option<String>
+    pub solo: Vec<String>,
 }
 
 impl ArgsParser {
     pub fn new(command_name: &str, description: &str) -> Self {
-        ArgsParser {
+        Self {
             command_name: command_name.to_owned(),
             description: description.to_owned(),
-            arguments: Vec::new()
+            arguments: Vec::new(),
         }
     }
 
@@ -45,9 +43,7 @@ impl ArgsParser {
 
     pub fn parse(&self, args: &[String]) -> Result<ArgResult, String> {
         let mut map = BTreeMap::new();
-        let mut nn = Vec::new();
-        let mut action = None;
-        let mut action_set = false;
+        let mut solo = Vec::new();
         let mut stop_parsing = false;
         let mut i = 0;
 
@@ -61,32 +57,31 @@ impl ArgsParser {
             }
 
             if !stop_parsing && arg.starts_with("--") && arg.len() > 2 {
-                let (name, value_from_arg) = if let Some(eq_pos) = arg.find('=') {
+                let (name, value_from_arg) = arg.find('=').map_or_else(|| {
+                    let name = &arg[2..];
+                    (name, None)
+                }, |eq_pos| {
                     let name = &arg[2..eq_pos];
                     let value = &arg[eq_pos + 1..];
                     (name, Some(value))
-                } else {
-                    let name = &arg[2..];
-                    (name, None)
-                };
+                });
 
                 if let Some(arg_def) = self.arguments.iter().find(|a| a.long == name) {
                     if arg_def.takes_value {
-                        if let Some(val) = value_from_arg {
-                            map.insert(name.to_owned(), val.to_owned());
-                        } else {
-                            if i + 1 < args.len() {
-                                let next = &args[i + 1];
-                                if !next.starts_with('-') || next == "--" {
-                                    map.insert(name.to_owned(), next.clone());
-                                    i += 1; // consume the value
-                                } else {
-                                    return Err(format!("option --{name} requires a value, but next argument is an option"));
-                                }
+                        let value = if let Some(val) = value_from_arg {
+                            val.to_owned()
+                        } else if i + 1 < args.len() {
+                            let next = &args[i + 1];
+                            if !next.starts_with('-') || next == "--" {
+                                i += 1;
+                                next.clone()
                             } else {
-                                return Err(format!("option --{name} requires a value, but none provided"));
+                                return Err(format!("option --{name} requires a value, but next argument is an option"));
                             }
-                        }
+                        } else {
+                            return Err(format!("option --{name} requires a value, but none provided"));
+                        };
+                        map.insert(name.to_owned(), value);
                     } else {
                         if value_from_arg.is_some() {
                             return Err(format!("flag --{name} does not take a value"));
@@ -103,6 +98,7 @@ impl ArgsParser {
             if !stop_parsing && arg.starts_with('-') && arg.len() > 1 {
                 let chars: Vec<char> = arg[1..].chars().collect();
                 let mut j = 0;
+                
                 while j < chars.len() {
                     let c = chars[j];
                     if let Some(arg_def) = self.arguments.iter().find(|a| a.short == Some(c)) {
@@ -111,24 +107,22 @@ impl ArgsParser {
                                 let value: String = chars[j + 1..].iter().collect();
                                 map.insert(arg_def.long.clone(), value);
                                 break;
-                            } else {
-                                if i + 1 < args.len() {
-                                    let next = &args[i + 1];
-                                    if !next.starts_with('-') || next == "--" {
-                                        map.insert(arg_def.long.clone(), next.clone());
-                                        i += 1; // consume the value
-                                    } else {
-                                        return Err(format!("option -{c} requires a value, but next argument is an option"));
-                                    }
+                            } else if i + 1 < args.len() {
+                                let next = &args[i + 1];
+                                if !next.starts_with('-') || next == "--" {
+                                    map.insert(arg_def.long.clone(), next.clone());
+                                    i += 1;
                                 } else {
-                                    return Err(format!("option -{c} requires a value, but none provided"));
+                                    return Err(format!("Option -{c} requires a value, but next argument is an option"));
                                 }
+                            } else {
+                                return Err(format!("Option -{c} requires a value, but none provided"));
                             }
                         } else {
                             map.insert(arg_def.long.clone(), "true".to_owned());
                         }
                     } else {
-                        return Err(format!("unknown short option: -{c}"));
+                        return Err(format!("Unknown short option: -{c}"));
                     }
                     j += 1;
                 }
@@ -136,19 +130,10 @@ impl ArgsParser {
                 continue;
             }
 
-            if !action_set {
-                action = Some(arg.clone());
-                action_set = true;
-            } else {
-                nn.push(arg.clone());
-            }
+            solo.push(arg.clone());
             i += 1;
         }
 
-        Ok(ArgResult {
-            map,
-            nn,
-            action,
-        })
+        Ok(ArgResult { map, solo })
     }
 }
