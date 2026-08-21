ecfb22e0e7b76521302146b12f106f226a2ddc5e
maslina524 <luk.kirill2804@gmail.com>
2026-08-21 23:22:17 +0300

    New fn encrypt in crc32

--- src/crc32.rs
+++ src/crc32.rs
@@ -1,4 +1,8 @@
+// SOURCE:
+// https://github.com/gcc-mirror/gcc/blob/master/libiberty/crc32.c
+
 static TABLE: [u32; 256] = generate_table();
+const INIT: u32 = 0;
 
 const fn generate_table() -> [u32; 256] {
     let mut table = [0u32; 256];
@@ -24,14 +28,35 @@ const fn generate_table() -> [u32; 256] {
     table
 }
 
+const fn encrypt(buf: &[u8]) -> u32 {
+    let mut crc = INIT;
+    let mut pos = 0usize;
+
+    while pos < buf.len() {
+        let index = ((crc >> 24) ^ buf[pos] as u32) & 255;
+        crc = (crc << 8) ^ TABLE[index as usize];
+        pos += 1;
+    }
+
+    crc
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::crc32::TABLE;
+    use crate::crc32;
 
     #[test]
     fn table_gen_test() {
-        for i in &TABLE {
+        for i in &crc32::TABLE {
             print!("{i:08x} ");
         }
+    }
+
+    #[test]
+    fn encrypt_test() {
+        let source = "Hello World";
+        let correct_hash = 0x4A_17_B1_56u32;
+        let my_hash = crc32::encrypt(source.as_bytes());
+        assert_eq!(correct_hash, my_hash);
     }
 }
