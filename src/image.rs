deb8aa8201f9f9109a52cdec3d77ab44476cac51
maslina524 <luk.kirill2804@gmail.com>
2026-08-22 20:30:29 +0300

    Added rgba color converter in image

--- src/image.rs
+++ src/image.rs
@@ -3,8 +3,94 @@ use alloc::{
     vec
 };
 
+#[derive(Debug, Clone, Copy)]
+pub enum ColorType {
+    Rgba,
+    Rgb,
+    GrayscaleAlpha,
+    Grayscale,
+    Indexed,
+    IndexedScaled
+}
+
+#[derive(Debug)]
+pub struct RgbaConvertError {
+    pub typ: ColorType,
+    pub bits: u8
+}
+
+impl core::fmt::Display for RgbaConvertError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f, "RgbaConvertError: nofetch does not support {:?} with {}-bit color depth, try converting the image to another format",
+            self.typ, self.bits
+        )
+    }
+}
+
+impl core::error::Error for RgbaConvertError {}
+
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct Rgba(pub u8, pub u8, pub u8, pub u8);
+
+impl Rgba {
+    pub const fn from(data: &[u8], typ: ColorType, bits: u8) -> Result<Self, RgbaConvertError> {
+        match (typ, bits) {
+            (ColorType::Rgba, 8) => {
+                Ok(Self(data[0], data[1], data[2], data[3]))
+            }
+            (ColorType::Rgba, 16) => {
+                let r = u16::from_be_bytes([data[0], data[1]]);
+                let g = u16::from_be_bytes([data[2], data[3]]);
+                let b = u16::from_be_bytes([data[4], data[5]]);
+                let a = u16::from_be_bytes([data[6], data[7]]);
+                Ok(Self((r >> 8) as u8, (g >> 8) as u8, (b >> 8) as u8, (a >> 8) as u8))
+            }
+            (ColorType::Rgb, 8) => {
+                Ok(Self(data[0], data[1], data[2], 255))
+            }
+            (ColorType::Rgb, 16) => {
+                let r = u16::from_be_bytes([data[0], data[1]]);
+                let g = u16::from_be_bytes([data[2], data[3]]);
+                let b = u16::from_be_bytes([data[4], data[5]]);
+                Ok(Self((r >> 8) as u8, (g >> 8) as u8, (b >> 8) as u8, 255))
+            }
+            (ColorType::Grayscale, 8) => {
+                let g = data[0];
+                Ok(Self(g, g, g, 255))
+            }
+            (ColorType::Grayscale, 16) => {
+                let g = u16::from_be_bytes([data[0], data[1]]);
+                let g8 = (g >> 8) as u8;
+                Ok(Self(g8, g8, g8, 255))
+            }
+            (ColorType::Grayscale, 1 | 2 | 4) => {
+                let val = data[0];
+                let g = match bits {
+                    1 => if val == 0 { 0 } else { 255 },
+                    2 => val * 85,
+                    4 => val * 17,
+                    _ => unreachable!(),
+                };
+                Ok(Self(g, g, g, 255))
+            }
+            (ColorType::GrayscaleAlpha, 8) => {
+                let g = data[0];
+                let a = data[1];
+                Ok(Self(g, g, g, a))
+            }
+            (ColorType::GrayscaleAlpha, 16) => {
+                let g = u16::from_be_bytes([data[0], data[1]]);
+                let a = u16::from_be_bytes([data[2], data[3]]);
+                let g8 = (g >> 8) as u8;
+                let a8 = (a >> 8) as u8;
+                Ok(Self(g8, g8, g8, a8))
+            }
+
+            _ => Err(RgbaConvertError { typ, bits }),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Image {
