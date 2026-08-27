0d17efd60676c9f25aa144facbdd9f1fb42b0bcd
maslina524 <luk.kirill2804@gmail.com>
2026-08-28 02:59:00 +0300

    Added last commit info in build.rs

--- build.rs
+++ build.rs
@@ -12,11 +12,83 @@ use zlib_rs::{
 };
 
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use regex::Regex;
 
 const VALID_CHARS: &[char] = &[
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_'
 ];
+
+struct Commit {
+    pub author: String,
+    pub email: String,
+    pub date: String,
+    pub sha: String,
+    pub sha_short: String,
+    pub message: String,
+    pub files: usize,
+    pub added: usize,
+    pub deleted: usize,
+    pub total: usize
+}
+
+impl Commit {
+    pub fn new() -> Self {
+        let log = Command::new("git")
+            .args([
+                "log",
+                "--format=%an%n%ae%n%ad%n%H%n%h%n%s",
+                "--date=\"format:%b %d %Y, %H:%M:%S\"", 
+                "-1"
+            ])
+            .output()
+            .ok()
+            .and_then(|output| String::from_utf8(output.stdout).ok())
+            .expect("Failed to call Git log");
+        
+        let log_parts: Vec<&str> = log.split('\n').collect();
+        assert!(log_parts.len() >= 6, "Failed to call Git log");
+        
+        let author = log_parts[0].to_owned();
+        let email = log_parts[1].to_owned();
+        let date = log_parts[2].to_owned();
+        let sha = log_parts[3].to_owned();
+        let sha_short = log_parts[4].to_owned();
+        let message = log_parts[5].to_owned();
+
+        let numstat = Command::new("git")
+            .args([
+                "log",
+                "--format=\"\"",
+                "--shortstat",
+                "-1"
+            ])
+            .output()
+            .ok()
+            .and_then(|output| String::from_utf8(output.stdout).ok())
+            .expect("Failed to call Git shortstat");
+        
+        let re = Regex::new(r"(\d+)").unwrap();
+        let nums: Vec<usize> = re
+            .find_iter(&numstat)
+            .filter_map(|m| m.as_str().parse().ok())
+            .collect();
+
+        assert!(nums.len() >= 3, "Failed to call Git shortstat");
+
+        let files = nums[0];
+        let added = nums[1];
+        let deleted = nums[2];
+        let total = added + deleted;
+
+        Self {
+            author, email, date,
+            sha, sha_short, message,
+            files, added, deleted, total
+        }
+    }
+}
+
 
 fn main() {
     // Bypasses caching, runs every time during compilation
@@ -56,8 +128,33 @@ fn main() {
     if let Some(idx) = rustc_version.find('(') {
         rustc_version = rustc_version[..idx - 1].trim().to_string();
     }
-
     println!("cargo:rustc-env=RUSTC_VERSION={}", rustc_version.trim());
+
+    // ENV: CARGO_VERSION
+    let mut cargo_version = Command::new("cargo")
+        .arg("--version")
+        .output()
+        .ok()
+        .and_then(|output| String::from_utf8(output.stdout).ok())
+        .unwrap_or_else(|| "unknown".to_string());
+    
+    if let Some(idx) = cargo_version.find('(') {
+        cargo_version = cargo_version[..idx - 1].trim().to_string();
+    }
+    println!("cargo:rustc-env=CARGO_VERSION={}", cargo_version.trim());
+
+    // ENV: Commit
+    let commit = Commit::new();
+    println!("cargo:rustc-env=COMMIT_AUTHOR={}",    commit.author);
+    println!("cargo:rustc-env=COMMIT_EMAIL={}",     commit.email);
+    println!("cargo:rustc-env=COMMIT_DATE={}",      commit.date);
+    println!("cargo:rustc-env=COMMIT_SHA={}",       commit.sha);
+    println!("cargo:rustc-env=COMMIT_SHA_SHORT={}", commit.sha_short);
+    println!("cargo:rustc-env=COMMIT_MESSAGE={}",   commit.message);
+    println!("cargo:rustc-env=COMMIT_FILES={}",     commit.files);
+    println!("cargo:rustc-env=COMMIT_ADDED={}",     commit.added);
+    println!("cargo:rustc-env=COMMIT_DELETED={}",   commit.deleted);
+    println!("cargo:rustc-env=COMMIT_TOTAL={}",     commit.total);
 
     // Compress logos
     let base_path = PathBuf::from("src/logo");
@@ -83,15 +180,15 @@ fn main() {
         let dest_path = PathBuf::from(dest_dir).join(path.file_name().unwrap());
         fs::write(dest_path, &*compressed).ok();
         
-        #[allow(clippy::cast_precision_loss)]
-        {
-            println!(
-                "cargo:warning={}: {}b -> {}b = {:.02}%",
-                path.display(),
-                content.len(), 
-                compressed.len(),
-                (compressed.len() as f64 / content.len() as f64).mul_add(-100.0, 100.0)
-            );
-        }
+        // #[allow(clippy::cast_precision_loss)]
+        // {
+        //     println!(
+        //         "cargo:warning={}: {}b -> {}b = {:.02}%",
+        //         path.display(),
+        //         content.len(), 
+        //         compressed.len(),
+        //         (compressed.len() as f64 / content.len() as f64).mul_add(-100.0, 100.0)
+        //     );
+        // }
     });
 }
