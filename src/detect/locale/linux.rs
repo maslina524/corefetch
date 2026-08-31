184be431a095c394026768d163f28e4a13b42894
maslina524 <luk.kirill2804@gmail.com>
2026-09-01 00:57:35 +0300

    Updated locale module in detect

---
+++ src/detect/locale/linux.rs
@@ -0,0 +1,16 @@
+use core::{ffi::CStr, ptr};
+
+use crate::{detect::locale::LocaleInfo, linux::libc::setlocale};
+
+const LC_ALL: i32 = 0;
+
+impl LocaleInfo {
+    pub fn new() -> Self {
+        setlocale(LC_ALL, c"".as_ptr());
+        let ptr = setlocale(LC_ALL, ptr::null());
+        let c_str = unsafe { CStr::from_ptr(ptr) };
+        let locale = c_str.to_string_lossy().into_owned();
+
+        Self { locale }
+    }
+}
