5dfce01eea4ccc36b430b5247e8dd9c13fbca36e
maslina524 <luk.kirill2804@gmail.com>
2026-08-31 04:35:12 +0300

    Added support cpu module in linux

---
+++ src/detect/cpu/linux.rs
@@ -0,0 +1,123 @@
+use alloc::vec::Vec;
+
+use crate::{
+    abort, 
+    detect::cpu::CpuInfo, 
+    linux::parser::LinuxInfo,
+    linux::fs,
+    linux::path::Path,
+    linux::parser::parse_range_notation,
+    format
+};
+
+impl CpuInfo {
+    pub fn new() -> Self {
+        let info = LinuxInfo::parse_cpu_info()
+            .unwrap_or_else(|e| abort!("Failed to open /proc/cpuinfo: {e}"));
+
+        let name = info.get("model name", "Unknown");
+        let logical_cores = Self::logical_cores_count();
+        let base_freq = info
+            .get("key", "0")
+            .parse::<f64>()
+            .unwrap_or(0.0) / 1000.0;
+
+        Self {
+            name,
+            vendor: Self::vendor(),
+            numa_nodes: Self::numa_nodes_count(),
+            physical_cores: Self::physical_cores_count(),
+            logical_cores,
+            online_cores: Self::online_cores_count(logical_cores),
+            packages: Self::package_count(),
+            code_name: Self::code_name(),
+            technology: Self::technology(),
+            base_freq,
+            temperature: Self::temperature(),
+            max_freq: Self::max_freq_formatted(),
+            logical_grouped: Self::logical_grouped(),
+            micro_arch: Self::micro_arch()
+        }
+    }
+
+    fn physical_cores_count() -> usize {
+        let mut ret = Vec::with_capacity(24);
+        let mut n = 0;
+        loop {
+            let path = Path::from(format!("/sys/devices/system/cpu/cpu{n}/topology/core_id"));
+            if !path.exists() {
+                break;
+            }
+            let content = fs::read_to_string(path).unwrap();
+            let id = if let Ok(n) = content.trim().parse::<usize>() {
+                n
+            } else {
+                continue;
+            };
+            if !ret.contains(&id) {
+                ret.push(id);
+            }
+
+            n += 1;
+        }
+
+        ret.len()
+    }
+
+    fn logical_cores_count() -> usize {
+        let mut n = 0;
+        loop {
+            let path = Path::from(format!("/sys/devices/system/cpu/cpu{n}/"));
+            if path.exists() {
+                n += 1;
+            } else {
+                break;
+            }
+        }
+        n
+    }
+
+    fn online_cores_count(logical_cores: usize) -> usize {
+        let content = fs::read_to_string("/sys/devices/system/cpu/online").unwrap();
+        let cores = parse_range_notation(&content, Some(logical_cores));
+        cores.len()
+    }
+
+    fn package_count() -> usize {
+        let mut ret = Vec::with_capacity(24);
+        let mut n = 0;
+        loop {
+            let path = Path::from(format!("/sys/devices/system/cpu/cpu{n}/topology/physical_package_id"));
+            if !path.exists() {
+                break;
+            }
+            let content = fs::read_to_string(path).unwrap();
+            let id = if let Ok(n) = content.trim().parse::<usize>() {
+                n
+            } else {
+                continue;
+            };
+            if !ret.contains(&id) {
+                ret.push(id);
+            }
+
+            n += 1;
+        }
+
+        ret.len()
+    }
+
+    fn numa_nodes_count() -> usize {
+        let mut n = 0;
+        loop {
+            let path = Path::from(format!("/sys/devices/system/node/node{n}"));
+            if !path.exists() {
+                break;
+            }
+
+            n += 1;
+        }
+
+        n
+    }
+}
