ea790f497c7755c24e8a3ef1d2c1da78fce02f9d
maslina524 <luk.kirill2804@gmail.com>
2026-09-01 18:58:24 +0300

    Updated todo crate, now the project can be compiled in release mode

--- todo/src/lib.rs
+++ todo/src/lib.rs
@@ -1,18 +1,37 @@
-#![feature(proc_macro_diagnostic)]
+#![cfg_attr(unstable, feature(proc_macro_diagnostic))]
+#![cfg_attr(unstable, feature(proc_macro_value))]
 
-use proc_macro::{Diagnostic, Level, Span, TokenStream};
-use syn::LitStr;
+#[cfg(unstable)]
+use proc_macro::{Diagnostic, Level, Span, TokenStream, TokenTree};
 
-#[proc_macro_attribute]
+#[cfg(not(unstable))]
+use proc_macro::TokenStream;
+
+fn get_message(attr: TokenStream) -> Option<String> {
+    attr.into_iter().next().and_then(|t| match t {
+        TokenTree::Literal(s) => {
+            let string = s.str_value().ok()?;
+            Some(format!("TODO: {string}"))
+        },
+        _ => None
+    })
+}
+
 #[allow(clippy::missing_panics_doc)]
+#[cfg(unstable)]
+#[proc_macro_attribute]
 pub fn todo(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let message = syn::parse::<LitStr>(attr).map_or_else(
-        |_| "TODO: Not implemented".to_owned(),
-        |l| format!("TODO: {}", l.value()),
-    );
+    let message = get_message(attr)
+        .unwrap_or_else(|| "TODO: Not implemented".to_owned());
 
     let span = Span::mixed_site();
     Diagnostic::spanned(span, Level::Warning, &message).emit();
 
+    item
+}
+
+#[cfg(not(unstable))]
+#[proc_macro_attribute]
+pub fn todo(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
