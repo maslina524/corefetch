d418403f1a6d56f142c04ed24a0de1cd1accb170
maslina524 <luk.kirill2804@gmail.com>
2026-09-07 22:50:50 +0300

    New fns {amd, intel}_name in detect/gpu

--- src/detect/gpu/mod.rs
+++ src/detect/gpu/mod.rs
@@ -94,16 +94,57 @@ impl GpuInfo {
     fn name(vendor_id: u32, device_id: u32) -> String {
         match vendor_id {
             0x10DE => NvidiaLib::get().device_name(),
-            0x15ad => Self::vmware_name(device_id).to_owned(),
+            0x15ad => format!("VMware {}", Self::vmware_name(device_id)),
+            0x1002 => format!("AMD {}", Self::amd_name(device_id)),
+            0x8086 => format!("Intel {}", Self::intel_name(device_id)),
             _ => "Unknown".to_owned(),
+        }
+    }
+
+    const fn amd_name(device_id: u32) -> &'static str {
+        match device_id {
+            0x1114 => "Krackan",
+            0x130F => "Kaveri",
+            0x13C0 => "Granite Ridge",
+            0x1435 => "Sephiroth",
+            0x150E => "Strix",
+            0x15BF => "Phoenix",
+            0x15D8 => "Picasso",
+            0x15DD => "Raven Ridge",
+            0x15E7 => "Barcelo",
+            0x1636 => "Renoir",
+            0x1638 => "Cezanne",
+            0x163F => "VanGogh",
+            0x164C => "Lucienne",
+            0x164E => "Raphael",
+            0x1681 => "Rembrandt",
+            0x1900 | 0x1901 => "HawkPoint",
+            _ => "Unknown"
+        }
+    }
+
+    const fn intel_name(device_id: u32) -> &'static str {
+        match device_id {
+            0x0166 => "HD Graphics 4000",
+            0x0412 => "HD Graphics 4600",
+            0x0A16 => "HD Graphics 4400",
+            0x5916 => "UHD Graphics 620",
+            0x5912 | 0x3E9B | 0x3E92 => "UHD Graphics 630",
+            0x8A52 => "Iris Plus Graphics",
+            0x9BC4 | 0x9A49 | 0x46A6 => "Iris Xe Graphics",
+            0x56A0 | 0x56A1 => "Arc A380",
+            0x56B0 => "Arc A580",
+            0x56C0 => "Arc A750",
+            0x56D0 => "Arc A770",
+            _ => "Unknown",
         }
     }
 
     const fn vmware_name(device_id: u32) -> &'static str {
         match device_id {
-            0x0405 => "VMware SVGA II",
-            0x0710 => "VMware SVGA",
-            _ => "VMware Unknown"
+            0x0405 => "SVGA II",
+            0x0710 => "SVGA",
+            _ => "Unknown"
         }
     }
 
